0dd4b033e758e1815424d8f20f998952149a64f0
Jessica Kranz <jessica98k@gmail.com>
2020-06-30 00:04:30 +0200

    bugfix race condition with multiple chat partners

---
+++ BusinessLogic/TcpConnection.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace CommonLogic
+{
+    public class TcpConnection
+    {
+        static bool go_on = true;
+        public static void Client(List<TcpClient> clients)
+        {
+            while (go_on)
+            {
+                Thread.Sleep(300);
+                string line = Console.ReadLine();
+
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(line);
+
+                foreach (var client in clients)
+                {
+                    try
+                    {
+                        NetworkStream stream = client.GetStream();
+
+                        // Send the message to the connected TcpServer.
+                        stream.Write(data, 0, data.Length);
+
+                        // Receive the TcpServer.response.
+
+                        // String to store the response ASCII representation.
+                        String responseData = String.Empty;
+
+                        // Read the first batch of the TcpServer response bytes.
+                        Int32 bytes = stream.Read(data, 0, data.Length);
+                        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                        Console.WriteLine("Received: {0}", responseData);
+
+                        //if (line.Equals("quit"))
+                        //{
+                        //    go_on = false;
+                        //    // Close everything.
+                        //stream.Close();
+                        //client.Close();
+                        //Console.WriteLine("Disconnected");
+                        //}
+                        //}
+
+                    }
+                    catch (ArgumentNullException e)
+                    {
+                        Console.WriteLine("ArgumentNullException: {0}", e);
+                    }
+                    catch (SocketException e)
+                    {
+                        Console.WriteLine("SocketException: {0}", e);
+                    }
+                }
+            }
+        }
+
+
+
+        public static void Server(Int32 port)
+        {
+            Console.WriteLine("Hello from the server");
+            TcpListener server = null;
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        //Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+
+    }
+}
--- PeerToPeerCloneA/Program.cs
+++ PeerToPeerCloneA/Program.cs
@@ -1,13 +1,9 @@
-﻿using Datenmodelle;
+﻿using CommonLogic;
+using Datenmodelle;
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Net;
 using System.Net.Sockets;
-using System.Reflection.Metadata.Ecma335;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace PeerToPeerCloneA
 {
@@ -22,17 +18,7 @@ namespace PeerToPeerCloneA
         private static string myName = peerID.ToString();
         private static List<Peer> bekanntePeers; //TODO Liste wo die Nachbarn abgespeichert sind. ABer wie ? Als Neighbours? neue Datenstrucktur? Nur IDs? auch IP's??
         private static List<Connection> neighbours;  //TODO Liste wo die Nachbarn abgespeichert sind. ABer wie ? Als Neighbours? neue Datenstrucktur? Nur IDs? auch IP's??
-        public static void ThreadProc1()
-        {
-            BeTheServer(13000);
-        }
 
-        public static void ThreadProc2()
-        {
-            BeTheServer(13003);
-        }
-
-        static bool go_on = true;
         static void Main(string[] args)
         {
             /*Nachrichtentest Testblock: */
@@ -86,173 +72,22 @@ namespace PeerToPeerCloneA
 
             }
 
-            Thread.CurrentThread.Name = "Main";
-            Thread t = new Thread(new ThreadStart(ThreadProc1));
-
-            t.Start();
-
-            Thread t1 = new Thread(new ThreadStart(ThreadProc2));
-            t1.Start();
 
 
-            //t.Join();
+            Thread server = new Thread(o => TcpConnection.Server(13000));
+            server.Start();
 
+            //Servers must be running before clients may connect
+            Thread.Sleep(1000);
 
-            Task taskConnect1;
-            Task taskConnect2;
-
-
-            while (true)
+            List<TcpClient> tcpClients = new List<TcpClient>
             {
-                taskConnect1 = new Task(() => Connect("127.0.0.1", 13001));
-                taskConnect1.Start();
-
-                taskConnect2 = new Task(() => Connect("127.0.0.1", 13002));
-                taskConnect2.Start();
-
-            }
-
-            taskConnect1.Wait();
-            taskConnect2.Wait();
-
+                new TcpClient("127.0.0.1", 13003)
+            };
+            Thread client = new Thread(o => TcpConnection.Client(tcpClients));
+            client.Start();
         }
 
-
-        static void Connect(String server, Int32 port)
-        {
-            try
-            {
-                // Create a TcpClient.
-                // Note, for this client to work you need to have a TcpServer
-                // connected to the same address as specified by the server, port
-                // combination.
-
-                TcpClient client = new TcpClient(server, port);
-
-                // Translate the passed message into ASCII and store it as a Byte array.
-                Byte[] data;
-
-                while (go_on)
-                {
-                    string line = Console.ReadLine();
-                    data = System.Text.Encoding.ASCII.GetBytes(line);
-
-                    // Get a client stream for reading and writing.
-                    //  Stream stream = client.GetStream();
-
-                    NetworkStream stream = client.GetStream();
-
-                    // Send the message to the connected TcpServer.
-                    stream.Write(data, 0, data.Length);
-
-                    //Console.WriteLine("Sent: {0}", line);
-
-                    // Receive the TcpServer.response.
-
-                    // Buffer to store the response bytes.
-                    data = new Byte[256];
-
-                    // String to store the response ASCII representation.
-                    String responseData = String.Empty;
-
-                    // Read the first batch of the TcpServer response bytes.
-                    Int32 bytes = stream.Read(data, 0, data.Length);
-                    responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                    Console.WriteLine("Received: {0}", responseData);
-
-                    if (line.Equals("quit"))
-                    {
-                        go_on = false;
-                        // Close everything.
-                        stream.Close();
-                        client.Close();
-                    }
-                }
-
-            }
-            catch (ArgumentNullException e)
-            {
-                Console.WriteLine("ArgumentNullException: {0}", e);
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-
-            Console.WriteLine("\n Disconnected");
-            Console.Read();
-        }
-
-
-
-        static void BeTheServer(Int32 port)
-        {
-            TcpListener server = null;
-            try
-            {
-                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-
-                // TcpListener server = new TcpListener(port);
-                server = new TcpListener(localAddr, port);
-
-                // Start listening for client requests.
-                server.Start();
-
-                // Buffer for reading data
-                Byte[] bytes = new Byte[256];
-                String data = null;
-
-                // Enter the listening loop.
-                while (true)
-                {
-                    Console.Write("Waiting for a connection... ");
-
-                    // Perform a blocking call to accept requests.
-                    // You could also use server.AcceptSocket() here.
-                    TcpClient client = server.AcceptTcpClient();
-                    Console.WriteLine("Connected!");
-
-                    data = null;
-
-                    // Get a stream object for reading and writing
-                    NetworkStream stream = client.GetStream();
-
-                    int i;
-
-                    // Loop to receive all the data sent by the client.
-                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                    {
-                        // Translate data bytes to a ASCII string.
-                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                        Console.WriteLine("Received: {0}", data);
-
-                        // Process the data sent by the client.
-                        data = data.ToUpper();
-
-                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                        // Send back a response.
-                        stream.Write(msg, 0, msg.Length);
-                        //Console.WriteLine("Sent: {0}", data);
-                    }
-
-                    // Shutdown and end connection
-                    client.Close();
-                }
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-            finally
-            {
-                // Stop listening for new clients.
-                server.Stop();
-            }
-
-            Console.WriteLine("\nHit enter to continue...");
-            Console.Read();
-        }
 
 
         const string PeerEntry = "PE"; //Eine Erfolgreiche PE Nachricht hat eine TTL "999x" wobei x auch ein Integer zwischen 1 und 9 ist. x gibt dabei die Zahl der Wunschnachbarn an.
