cbf93ab197dfa4b82642668e864e0b27105ae3a5
Leonard-Hangen <56963352+Leonard-Hangen@users.noreply.github.com>
2020-06-30 18:38:28 +0200

    Just a small step it seems. But this is huge! We will get there this will work!
    Also did you know that: "Manche Schlafanzüge haben Streifen. Andere Widerum nicht!. Ist das nicht interessant?"

--- Datenmodelle/Connection.cs
+++ Datenmodelle/Connection.cs
@@ -7,7 +7,7 @@ namespace Datenmodelle
 {
     public class Connection
     {
-        Peer partnerPeer;
+        public Peer partnerPeer;
         public IPAddress partnerIPAddress;
         int partnerPortNr;
 
@@ -26,27 +26,6 @@ namespace Datenmodelle
             this.mySelf = mySelf;
             this.myIPAddress = myIPAddress;
             this.myPortNr = myPortNr;
-        }
-
-        /// <summary>
-        /// This was created Only for Test Cases and should soon seice existance. 
-        /// </summary>
-        /// <param name="dummy"></param>
-        public Connection(string dummy)
-        {
-            if (dummy != "dummy")
-            {
-                throw new System.ArgumentException("You should not execute this code. This is just for testing purposes. If you want to test. Use \"dummy\" as the string input");
-            }
-            Random rand = new Random();
-            this.partnerPeer = new Peer(rand.Next(1 * (int)Math.Pow(10, 7), 1 * (int)Math.Pow(10, 8) - 1));
-            //this.partnerIPAddress = "dummy";
-            //this.partnerPortNr = "dummy";
-
-            this.mySelf = new Peer(rand.Next(1 * (int)Math.Pow(10, 7), 1 * (int)Math.Pow(10, 8) - 1));
-            //this.myIPAddress = "dummy";
-            //this.myPortNr = "dummy";
-
         }
     }
 }
